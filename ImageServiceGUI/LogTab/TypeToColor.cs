eafc4d0ad3a8c63edea0d3b94899e5b4e4fab50a
Uriel <Uriel@DESKTOP-G7RPEHC>
2018-05-22 22:45:35 +0300

    C

---
+++ ImageServiceGUI/LogTab/TypeToColor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace ImageServiceGUI.LogTab
+{
+    class TypeToColor : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string val = value.ToString();
+            switch (val)
+            {
+                case "Information":
+                    return "LightGreen";
+                case "1":
+                    return "Yellow";
+                case "2":
+                    return "Red";
+                default:
+                    return "White";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
