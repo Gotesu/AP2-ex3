7709e6d97fbef90878c9a13fa241ad37944319b1
Uriel <Uriel@DESKTOP-G7RPEHC>
2018-06-07 23:16:04 +0300

    nicey nice

---
+++ ImageServiceWeb/Models/ConfigModel.cs
@@ -0,0 +1,24 @@
+﻿using GUICommunication.Client;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ImageServiceWeb.Models
+{
+    public class ConfigModel
+    {
+        private IGUIClient client;
+
+        public ConfigModel()
+        {
+            client = GUIClient.Instance();
+        }
+
+        public JObject getConfig()
+        {
+            return client.
+        }
+    }
+}
