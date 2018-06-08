a8faf7f980ac8032b01d420eb770eee73f223f04
Uriel <Uriel@DESKTOP-G7RPEHC>
2018-06-08 15:41:20 +0300

    log entries base

---
+++ ImageServiceWeb/Models/Entry.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ImageServiceWeb.Models
+{
+    public class Entry
+    {
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "Type")]
+        public string Type { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "Message")]
+        public string Message { get; set; }
+    }
+}
