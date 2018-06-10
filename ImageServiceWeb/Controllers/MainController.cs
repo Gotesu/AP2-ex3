0838e1b8af8014efce1f69c7b19af4cd0c3b3a10
Uriel <Uriel@DESKTOP-G7RPEHC>
2018-06-10 22:55:53 +0300

    arranged it for you

--- ImageServiceWeb/Controllers/MainController.cs
+++ ImageServiceWeb/Controllers/MainController.cs
@@ -17,14 +17,6 @@ namespace ImageServiceWeb.Controllers
         static HomeModel model = new HomeModel();
         static ConfigModel configModel = new ConfigModel();
         static LogModel logModel = new LogModel();
-        static List<Employee> employees = new List<Employee>()
-        {
-          new Employee  { FirstName = "Moshe", LastName = "Aron", Email = "Stam@stam", Salary = 10000, Phone = "08-8888888" },
-          new Employee  { FirstName = "Dor", LastName = "Nisim", Email = "Stam@stam", Salary = 2000, Phone = "08-8888888" },
-          new Employee   { FirstName = "Mor", LastName = "Sinai", Email = "Stam@stam", Salary = 500, Phone = "08-8888888" },
-          new Employee   { FirstName = "Dor", LastName = "Nisim", Email = "Stam@stam", Salary = 20, Phone = "08-8888888" },
-          new Employee   { FirstName = "Dor", LastName = "Nisim", Email = "Stam@stam", Salary = 700, Phone = "08-8888888" }
-        };
         static List<Entry> entries = new List<Entry>();
         // GET: First
         public ActionResult Config()
@@ -56,6 +48,12 @@ namespace ImageServiceWeb.Controllers
         }
 
         [HttpGet]
+        public ActionResult Photos()
+        {
+            return View();
+        }
+
+        [HttpGet]
         public JObject GetStatus()
         {
             JObject data = new JObject();
@@ -67,22 +65,7 @@ namespace ImageServiceWeb.Controllers
             return data;
         }
 
-        [HttpPost]
-        public JObject GetEmployee(string name, int salary)
-        {
-            foreach (var empl in employees)
-            {
-                if (empl.Salary > salary || name.Equals(name))
-                {
-                    JObject data = new JObject();
-                    data["FirstName"] = empl.FirstName;
-                    data["LastName"] = empl.LastName;
-                    data["Salary"] = empl.Salary;
-                    return data;
-                }
-            }
-            return null;
-        }
+        
 
         // GET: First/Details
         public ActionResult Log()
@@ -111,78 +94,6 @@ namespace ImageServiceWeb.Controllers
                 entries.Add(entry);
             }
             return View(entries);
-        }
-
-        // GET: First/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: First/Create
-        [HttpPost]
-        public ActionResult Create(Employee emp)
-        {
-            try
-            {
-                employees.Add(emp);
-
-                return RedirectToAction("Details");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: First/Edit/5
-        public ActionResult Edit(int id)
-        {
-            foreach (Employee emp in employees) {
-                if (emp.ID.Equals(id)) { 
-                    return View(emp);
-                }
-            }
-            return View("Error");
-        }
-
-        // POST: First/Edit/5
-        [HttpPost]
-        public ActionResult Edit(int id, Employee empT)
-        {
-            try
-            {
-                foreach (Employee emp in employees)
-                {
-                    if (emp.ID.Equals(id))
-                    {
-                        emp.copy(empT);
-                        return RedirectToAction("Config");
-                    }
-                }
-
-                return RedirectToAction("Config");
-            }
-            catch
-            {
-                return RedirectToAction("Error");
-            }
-        }
-
-        // GET: First/Delete/5
-        public ActionResult Delete(int id)
-        {
-            int i = 0;
-            foreach (Employee emp in employees)
-            {
-                if (emp.ID.Equals(id))
-                {
-                    employees.RemoveAt(i);
-                    return RedirectToAction("Details");
-                }
-                i++;
-            }
-            return RedirectToAction("Error");
         }
 
         public ActionResult HandlerRemoval(string path)
